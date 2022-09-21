e10057451e5e69d6a203afbd1ff217479473a06e
Gabriel Cancro <gabrielcancro@3ogs.com.ar>
2022-09-21 17:45:49 -0300

    update

--- server.js
+++ server.js
@@ -6,7 +6,27 @@ const app = express();
 app.use(cors({
   origin: 'https://timeapi.io'
 }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use( express.static('public') );
+
+app.get('/time', function (req, res, next) { // GET 'http://www.example.com/admin/new?a=b'
+  console.dir(req.originalUrl) // '/admin/new?a=b' (WARNING: beware query string)
+  console.dir(req.baseUrl) // '/admin'
+  console.dir(req.path) // '/new'
+  console.dir(req.baseUrl + req.path) // '/admin/new' (full path without query string)
+  let ts = Date.now();
+  res.send({time:ts})
+  
+
+int(time.time()) 
+})
+
  
 // Start the server
 const PORT = process.env.PORT || 8080;
